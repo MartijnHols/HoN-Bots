060434650b6e132ce0c2a7fc950d3467e47a5a15
Zerotorescue <zero@isimba.org>
2013-06-05 16:27:17 +0200

    Cleaning Behavior.class.lua up (no actual changes to functionality): Disabled bDebug by default. Converted all string copiers to string.formats. Fixed comment blocks describing functions.

--- Behaviors/Behavior.class.lua
+++ Behaviors/Behavior.class.lua
@@ -1,7 +1,7 @@
 local _G = getfenv(0);
 local setmetatable, tinsert, tcopy, type, strlen, strformat = _G.setmetatable, _G.table.insert, _G.table.copy, _G.type, _G.string.len, _G.string.format;
 
-local bDebug = true;
+local bDebug = false;
 
 -- A namespace to contain Bot classes
 -- This is required so our classes don't polute the global namespace and so that they are harder to accidentally override (if Behavior
@@ -15,18 +15,18 @@ class.__index = class;
 class.__Name = 'Unnamed';
 class.__IsEnabled = true;
 
---[[ function class.Create(name) (constructor)
+--[[ function Behavior.Create(name) (constructor)
 description:		Creates a new instance of the class.
-parameters:			name				(string) The name of the behavior. Must be without Behavior at the end and must be unique.
+parameters:			name				(String) The name of the behavior. Must be without Behavior at the end and must be unique.
 returns:			(Behavior) A new instance of the class.
 ]]
 function class.Create(name)
 	if not name then
-		error('Missing parameter 1: name.');
+		error(strformat('Missing parameter 1: name.'));
 	elseif type(name) ~= 'string' or strlen(name) < 3 then
-		error('Parameter 1 (' .. tostring(name) .. ') must be a string of at least 3 characters.');
+		error(strformat('Parameter 1 (%s) must be a string of at least 3 characters.', tostring(name)));
 	elseif string.find(name:lower(), 'behavior') or string.find(name:lower(), 'behaviour') then
-		error('Behavior name (' .. name .. ') shouldn\'t contain the word "Behavior".');
+		error(strformat('Behavior name (%s) shouldn\'t contain the word "Behavior".', name));
 	end
 	local bhvr = {};
 	setmetatable(bhvr, class);
@@ -36,7 +36,7 @@ function class.Create(name)
 	return bhvr;
 end
 
---[[ abstract function class:Utility(botBrain)
+--[[ abstract function Behavior:Utility(botBrain)
 description:		The utility function checks if the behavior should be executed. This is different for either a BehaviorRunner or 
 					a TeamBotBrain;
 					 - A BehaviorRunner will run the utility functions for all behaviors and execute the behavior returning the 
@@ -47,27 +47,27 @@ parameters:			botBrain			(CBotBrain) The bot brain of the bot. This is only pass
 returns:			(Number) A number indicating the current utility value. If 0 the utility will be ignored.
 ]]
 function class:Utility(botBrain)
-	error(self.Name .. 'Behavior Utility function hasn\'t been implemented!');
+	error(strformat('%s Utility function hasn\'t been implemented!', self:GetFullName()));
 	return 0;
 end
 
---[[ abstract function class:Execute(botBrain)
+--[[ abstract function Behavior:Execute(botBrain)
 description:		The execute function is executed if the utility function returned the highest utility value. In case the behavior 
 					is running in a TeamBotBrain it will be executed whenever the utility value is above 0.
 parameters:			botBrain			(CBotBrain) The bot brain of the bot. This is only passed by a behavior runner.
 ]]
 function class:Execute(botBrain)
-	error(self.Name .. 'Behavior Execute function hasn\'t been implemented!');
+	error(strformat('%s Execute function hasn\'t been implemented!', self:GetFullName()));
 end
 
---[[ function class:GetName()
+--[[ function Behavior:GetName()
 description:		Get the name of this Behavior.
 returns:			(String) The name of this behavior as provided in the Create function.
 ]]
 function class:GetName()
 	return self.__Name;
 end
---[[ function class:GetFullName()
+--[[ function Behavior:GetFullName()
 description:		Get the full name of this Behavior.
 returns:			(String) The full name of this Behavior, including the word Behavior at the end.
 ]]
@@ -75,34 +75,34 @@ function class:GetFullName()
 	return strformat('%sBehavior', self.__Name);
 end
 
---[[ function class:Enable()
+--[[ function Behavior:Enable()
 description:		Enable this behavior.
 ]]
 function class:Enable()
 	if bDebug then
-		Echo('^wBehavior.class: Enabling ^y' .. self:GetFullName() .. '^w.');
+		Echo(strformat('^wBehavior.class: Enabling ^y%s^w.', self:GetFullName()));
 	end
 	
 	self.__IsEnabled = true;
 end
---[[ function class:Disable()
+--[[ function Behavior:Disable()
 description:		Disable this behavior.
 ]]
 function class:Disable()
 	if bDebug then
-		Echo('^wBehavior.class: Disabling ^y' .. self:GetFullName() .. '^w.');
+		Echo(strformat('^wBehavior.class: Disabling ^y%s^w.', self:GetFullName()));
 	end
 	
 	self.__IsEnabled = false;
 end
---[[ function class:IsEnabled()
+--[[ function Behavior:IsEnabled()
 description:		Check if this behavior is enabled.
 returns:			(Boolean) Returns if the Behavior is enabled.
 ]]
 function class:IsEnabled()
 	return self.__IsEnabled;
 end
---[[ function class:__tostring()
+--[[ function Behavior:__tostring()
 description:		Make a string representation of this behavior.
 returns:			(String) An identifier of this Behavior.
 ]]
@@ -110,29 +110,29 @@ function class:__tostring()
 	return strformat('<%s>', self:GetFullName());
 end
 
---[[ function class:AddToLegacyBehaviorRunner(behaviorRunner)
+--[[ function Behavior:AddToLegacyBehaviorRunner(behaviorRunner, bOverride)
 description:		Add this behavior to a legacy behavior runner. We can't add this behavior instance to the old behaviorLib since it 
 					calls .Utility rather then :Utility which doesn't pass the self var properly. So we create a wrapper table and add 
 					that instead.
-parameters:			behaviorRunner		(BehaviorLib) The behavior runner.
+parameters:			behaviorRunner		(BehaviorLib) The legacy behavior runner.
 					bOverride			(Boolean) True to allow overriding of the behavior.
 ]]
 function class:AddToLegacyBehaviorRunner(behaviorRunner, bOverride)
 	if not behaviorRunner or not behaviorRunner.tBehaviors then
-		error('Behavior.class: Provided behavior runner "' .. tostring(behaviorRunner) .. '" is invalid.');
+		error(strformat('Behavior.class: Provided behavior runner "%s" is invalid.', tostring(behaviorRunner)));
 	elseif not bOverride and behaviorRunner[self:GetFullName()] then
-		error('Behavior.class: Behavior runner "' .. tostring(behaviorRunner) .. '" already has a "' .. self:GetFullName() .. '".');
+		error(strformat('Behavior.class: Behavior runner "%s" already has a "%s".', tostring(behaviorRunner), self:GetFullName()));
 	end
 	
 	for i = 1, #behaviorRunner.tBehaviors do
 		if behaviorRunner.tBehaviors[i].Name == self.__Name then
-			error('Behavior.class: Not adding "' .. self:GetFullName() .. '" to behaviors: found existing behavior with matching name.');
+			error(strformat('Behavior.class: Not adding "%s" to behaviors: found existing behavior with matching name.', self:GetFullName()));
 			return;
 		end
 	end
 	
 	if bDebug then
-		Echo('^wBehavior.class: Adding ^y' .. self:GetFullName() .. '^w to behavior runner ^y' .. tostring(behaviorRunner) .. '^w.');
+		Echo(strformat('^wBehavior.class: Adding ^y%s^w to behavior runner ^y%s^w.', self:GetFullName(), tostring(behaviorRunner)));
 	end
 	
 	-- Keep a reference available for others to access
@@ -161,21 +161,20 @@ function class:AddToLegacyBehaviorRunner(behaviorRunner, bOverride)
 	tinsert(behaviorRunner.tBehaviors, tBhvr);
 end
 
---[[ function class:AddToLegacyTeamBotBrain(teamBot)
+--[[ function Behavior:AddToLegacyTeamBotBrain(teamBot, bOverride)
 description:		Add this behavior to the legacy TeamBotBrain.
 parameters:			teamBot				(TeamBotBrain) The TeamBotBrain to add to.
 					bOverride			(Boolean) True to allow overriding of the behavior.
 ]]
 function class:AddToLegacyTeamBotBrain(teamBot, bOverride)
 	if not teamBot or not teamBot.onthink then
-		error('Behavior.class: Provided teambot "' .. tostring(teamBot) .. '" is invalid.');
+		error(strformat('Behavior.class: Provided teambot "%s" is invalid.', tostring(teamBot)));
 	elseif not bOverride and teamBot[self:GetFullName()] then
-		local sTeamBotName = teamBot.myName or tostring(teamBot);
-		error('Behavior.class: Teambot "' .. sTeamBotName .. '" already has a "' .. self:GetFullName() .. '".');
+		error(strformat('Behavior.class: Teambot "%s" already has a "%s".', (teamBot.myName or tostring(teamBot)), self:GetFullName()));
 	end
 	
 	if bDebug then
-		Echo('^wBehavior.class: Adding ^y' .. self:GetFullName() .. '^w to TeamBotBrain ^y' .. teamBot.myName .. '^w.');
+		Echo(strformat('^wBehavior.class: Adding ^y%s^w to TeamBotBrain ^y%s^w.', self:GetFullName(), (teamBot.myName or tostring(teamBot))));
 	end
 	
 	-- Keep a reference available for others to access
